eccd334073d09cf09934b651f8e8d21a2bc73825
Peter Ambrose <pambros@localhost.localdomain>
2024-11-05 15:11:12 +0000

    add unit tests

--- src/main/java/pambros/BgpUtils.java
+++ src/main/java/pambros/BgpUtils.java
@@ -1,25 +1,66 @@
 package pambros;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class BgpUtils {
-	
-	public Collection<String> parse(final String asnPath) {
 
-		List<String[]> breakdown = breakdown(asnPath);
-		
-		Collection<String> paths = new LinkedList<>();
-		return paths;
-		
+	public Collection<String> parse(final String asnPath) {
+		if (asnPath == null) {
+			return Collections.emptyList();
+		}
+		final List<String[]> pathElements = breakdown(asnPath);
+		final Collection<String> newPaths = new LinkedList<>();
+		buildPaths(pathElements, 0, new Stack<String>(), newPaths);
+		return newPaths;
 	}
 
-	private List<String[]> breakdown(String asnPath) {
-		List<String[]> breakDown = new LinkedList<>();
+	private void buildPaths(final List<String[]> pathElements,
+			                final int pathElementIndex,
+			                final Stack<String> asnPath,
+			                final Collection<String> allNewPaths) {
+		if (pathElementIndex >= pathElements.size()) {
+			if (!asnPath.isEmpty()) {
+				allNewPaths.add(toSpaceSeparatedString(asnPath));
+			}
+			return;
+		}
+
+		boolean doPop = false;
+		final String[] asnsAtIndex = pathElements.get(pathElementIndex);
+		for (final String asn : asnsAtIndex) {
+			// subsequent asn in this group, remove previous
+			if (doPop) {
+				asnPath.pop();
+				doPop = false;
+			}
+			// add this asn to path if not the same as the latest in path
+			if (asnPath.size() == 0 || !asnPath.peek().equals(asn)) {
+				asnPath.push(asn);
+				doPop = true;
+			}
+
+			// next asn item/group
+			buildPaths(pathElements, pathElementIndex + 1, asnPath, allNewPaths);
+		}
+
+		// when leaving, pop the item I have probably just added
+		if (doPop) {
+			asnPath.pop();
+		}
+
+	}
+
+
+	private List<String[]> breakdown(final String asnPath) {
+		final List<String[]> breakDown = new ArrayList<>(20);
 		boolean parsingGroup = false;
-		StringBuilder token = new StringBuilder();
-		for (char c : asnPath.toCharArray()) {
+		final StringBuilder token = new StringBuilder();
+		for (final char c : asnPath.toCharArray()) {
 			if (c == ' ') {
 				if (!parsingGroup && !token.isEmpty()) {
 					breakDown.add(new String[] {token.toString()});
@@ -42,8 +83,19 @@ public class BgpUtils {
 		if (token.length() > 0) {
 			breakDown.add(new String[] {token.toString()});
 		}
-		
+
 		return breakDown;
+	}
+
+	private String toSpaceSeparatedString(final Collection<String> strings) {
+		final StringBuilder builder = new StringBuilder();
+		for (final String str : strings) {
+			if (builder.length() > 0) {
+				builder.append(" ");
+			}
+			builder.append(str);
+		}
+		return builder.toString();
 	}
 
 }
--- src/test/java/pambros/BgpUtilsTest.java
+++ src/test/java/pambros/BgpUtilsTest.java
@@ -1,8 +1,9 @@
 package pambros;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 import org.junit.Test;
 
@@ -10,18 +11,111 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class BgpUtilsTest 
+public class BgpUtilsTest
 {
 
-	private BgpUtils toTest = new BgpUtils();
-	
-    /**
-     * 
-     */
+	private final BgpUtils toTest = new BgpUtils();
+
 	@Test
-    public void testApp()
+	public void testEmpty() {
+		Collection<String> paths = toTest.parse("");
+		assertEquals(0, paths.size());
+
+		paths = toTest.parse("    ");
+		assertEquals(0, paths.size());
+
+		paths = toTest.parse(null);
+		assertEquals(0, paths.size());
+	}
+
+	@Test
+	public void testSimplePath() {
+		Collection<String> paths = toTest.parse("111");
+		assertEquals(1, paths.size());
+		Iterator<String> it = paths.iterator();
+		assertEquals("111", it.next());
+
+		paths = toTest.parse("222 333");
+		assertEquals(1, paths.size());
+		it = paths.iterator();
+		assertEquals("222 333", it.next());
+
+		paths = toTest.parse("444 555 666");
+		assertEquals(1, paths.size());
+		it = paths.iterator();
+		assertEquals("444 555 666", it.next());
+	}
+
+
+	@Test
+	public void testSimplePathRepeatingAsns() {
+		Collection<String> paths = toTest.parse("111 111");
+		assertEquals(1, paths.size());
+		Iterator<String> it = paths.iterator();
+		assertEquals("111", it.next());
+
+		paths = toTest.parse("222 222 333 333");
+		assertEquals(1, paths.size());
+		it = paths.iterator();
+		assertEquals("222 333", it.next());
+
+		paths = toTest.parse("444 444 555 666 666");
+		assertEquals(1, paths.size());
+		it = paths.iterator();
+		assertEquals("444 555 666", it.next());
+	}
+
+	@Test
+    public void testSingleAsnGroup()
     {
-    	Collection<String> paths = toTest.parse("111  {222, 333} 444 {555,666} 777");
-        assertTrue( true );
+    	final Collection<String> paths = toTest.parse("111 {222, 333}");
+		assertEquals(2, paths.size());
+		final Iterator<String> it = paths.iterator();
+		assertEquals("111 222", it.next());
+		assertEquals("111 333", it.next());
     }
+
+	@Test
+    public void testMultipleAsnGroup()
+    {
+    	final Collection<String> paths = toTest.parse("111 {222,333} 444 {555,666,777}");
+		assertEquals(6, paths.size());
+		final Iterator<String> it = paths.iterator();
+		assertEquals("111 222 444 555", it.next());
+		assertEquals("111 222 444 666", it.next());
+		assertEquals("111 222 444 777", it.next());
+		assertEquals("111 333 444 555", it.next());
+		assertEquals("111 333 444 666", it.next());
+		assertEquals("111 333 444 777", it.next());
+    }
+
+	@Test
+    public void testMultipleAsnGroupExtraWhitespace()
+    {
+    	final Collection<String> paths = toTest.parse("111  {222,  333}  444 { 555, 666, 777 }");
+		assertEquals(6, paths.size());
+		final Iterator<String> it = paths.iterator();
+		assertEquals("111 222 444 555", it.next());
+		assertEquals("111 222 444 666", it.next());
+		assertEquals("111 222 444 777", it.next());
+		assertEquals("111 333 444 555", it.next());
+		assertEquals("111 333 444 666", it.next());
+		assertEquals("111 333 444 777", it.next());
+    }
+
+
+	@Test
+    public void testMultipleAsnGroupRepeatingAsns()
+    {
+    	final Collection<String> paths = toTest.parse("111 {111,333} 444 {555,444,777} 888");
+		assertEquals(6, paths.size());
+		final Iterator<String> it = paths.iterator();
+		assertEquals("111 444 555 888", it.next());
+		assertEquals("111 444 888", it.next());
+		assertEquals("111 444 777 888", it.next());
+		assertEquals("111 333 444 555 888", it.next());
+		assertEquals("111 333 444 888", it.next());
+		assertEquals("111 333 444 777 888", it.next());
+    }
+
 }
